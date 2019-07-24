13f4ee2d2e4585d96e121cc6f505c27a4f4ac23c
angelonios <angel8892@email.cz>
2019-07-24 14:03:54 +0200

    repaired eventHandeling at selectCell, added highlight all cells with same number functionality.

--- src/sudoku/Controller.java
+++ src/sudoku/Controller.java
@@ -3,13 +3,12 @@ package sudoku;
 import cell.interfaces.ICell;
 import grid.objects.GridManager;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.InputEvent;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import sudoku.objects.CellModel;
 
@@ -31,25 +30,53 @@ public class Controller {
     GridPane board;
 
     @FXML
-    private void selectCell(Event selectedCell){
-        try{
-            if(currentCell == null){
-                currentCell = (StackPane) selectedCell.getTarget();
-                currentCell.setStyle("-fx-border-color: red; -fx-border-width: 2.25px;");
-            } else{
-                currentCell.setStyle(null);
-                currentCell = (StackPane) selectedCell.getTarget();
-                currentCell.setStyle("-fx-border-color: red; -fx-border-width: 2.25px;");
-            }
-        }catch (Exception e){
-            System.out.println("Got an error, sir! : \n" + e.getMessage());
-            e.printStackTrace();
+    private void selectCell(InputEvent selectedCell){
+        if(!selectedCell.getTarget().getClass().toString().equals("class javafx.scene.layout.StackPane")){
+            return;
+        }
+        resetCellBackgroud();
+        StackPane target = (StackPane) selectedCell.getTarget();
+        if(target.getUserData().toString().equals("writable")){
+            setCurrentCell(target);
+        } else{
+            getCellsWithSameNumber(target);
+        }
+    }
+
+    private void getCellsWithSameNumber(StackPane target) {
+        resetCellBackgroud();
+        String number = getText(target.getChildren()).getText();
+        getStackPaneList().filter(stackPane -> getText(stackPane.getChildren()).getText().equals(number))
+                          .forEach(stackPane -> stackPane.setStyle("-fx-background-color: red;"));
+    }
+
+    private void resetCellBackgroud(){
+        getStackPaneList().forEach(stackPane -> stackPane.setStyle(null));
+    }
+
+    private void setCurrentCell(StackPane target){
+        if(currentCell == null){
+            currentCell = target;
+            currentCell.setStyle("-fx-border-color: red; -fx-border-width: 2.25px;");
+        } else{
+            currentCell.setStyle(null);
+            currentCell = target;
+            currentCell.setStyle("-fx-border-color: red; -fx-border-width: 2.25px;");
         }
     }
 
     @FXML
     private void enterNumberFromNumpad(Event buttonClicked){
-        System.out.println(buttonClicked.getTarget());
+        if(!buttonClicked.getSource().getClass().toString().equals("class javafx.scene.control.Button")){
+            return;
+        }
+        Button source = (Button) buttonClicked.getSource();
+        if(currentCell == null){
+            return;
+        } else{
+            getText(currentCell.getChildren()).setText((source.getText()));
+        }
+
     }
 
     @FXML
@@ -67,24 +94,28 @@ public class Controller {
             int number = cellModel.getNumber();
             if(number == 0){
                 listOfTexts.get(index).setText("");
+                listOfStackPanes.get(index).setUserData("writable");
             } else{
                 listOfTexts.get(index).setText(String.valueOf(number));
-                listOfStackPanes.get(index).setDisable(true);
+                listOfTexts.get(index).setStyle("-fx-font-weight: bold");
+                listOfStackPanes.get(index).setUserData("fixed");
             }
         });
     }
 
     private void initModel(List<ICell> cells){
         cells.stream().map(this::makeCellModel).collect(Collectors.toList()).stream()
-                .forEach(
-                        cellModel -> cellModel.numberProperty().addListener(
-                                (observable, oldValue, newValue) -> getStackPaneList().map(stackpane -> getText(stackpane.getChildren()))
-                                        .forEach(text -> text.setText(String.valueOf(cellModel.getNumber())))));
-
+             .forEach(cellModel -> cellModel.numberProperty()
+                                            .addListener((observable, oldValue, newValue) -> getStackPaneList()
+                                                                                             .map(stackpane -> getText(stackpane.getChildren()))
+                                                                                             .forEach(text -> text.setText(String.valueOf(cellModel.getNumber())))));
     }
 
     private CellModel makeCellModel(ICell cell){
-        cellModels.add(new CellModel(cell.getRightNumber().intValue(), cell.getNumber().intValue(), cell.getPoint().getRow().intValue(), cell.getPoint().getCol().intValue()));
+        cellModels.add(new CellModel(cell.getRightNumber(),
+                                     cell.getNumber(),
+                                     cell.getPoint().getRow(),
+                                     cell.getPoint().getCol()));
         return cellModels.get(0);
     }
 
@@ -94,7 +125,8 @@ public class Controller {
     }
 
     private Text getText(ObservableList<Node> children){
-        children.stream().forEach(child -> System.out.println(child.getClass().toString()));
-        return children.stream().filter(thing -> thing.getClass().toString().equals("class javafx.scene.text.Text")).map(cellText -> (Text) cellText).collect(Collectors.toList()).get(0);
+        return children.stream().filter(thing -> thing.getClass().toString().equals("class javafx.scene.text.Text"))
+                                .map(cellText -> (Text) cellText)
+                                .collect(Collectors.toList()).get(0);
     }
 }
